daf178c5e1a83bf0135446e51c28f0a6732bbaed
Alex Ortega <alexdiclothingco@icloud.com>
2022-06-15 17:01:18 -0600

    updated js.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import mysql from 'mysql2';
-// import { EmployeeTracker } from './lib/employeetracker.js'
+import { employeetracker } from './lib/employeetracker.js'
 
 const init = () => {
     const sqlConnect = {
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'August114!',
         database: 'employeetracker_db'
     }
 
     const db = mysql.createConnection(sqlConnect, console.log(`\n...connected to employee tracker database\n`));
-    const employeeTracker = new EmployeeTracker(db);
+    const employeeTracker = new employeetracker(db);
     employeeTracker.init();
 }
 
-init();
+init(); 
